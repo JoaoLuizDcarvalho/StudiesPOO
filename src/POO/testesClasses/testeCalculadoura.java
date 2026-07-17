5c5ba9f11e4b3f220214c5aa2e2b270299df7db8
Joao Luiz Nicolau <joaolcn231@gmail.com>
2026-07-16 22:51:25 -0300

    Inicio de Parametros

---
+++ src/POO/testesClasses/testeCalculadoura.java
@@ -0,0 +1,13 @@
+package POO.testesClasses;
+
+import POO.MetodosDominio.Calculadoura;
+
+public class testeCalculadoura {
+    static void main(String[] args) {
+
+        Calculadoura calculadoura = new Calculadoura();
+            calculadoura.valorDeSoma();
+            calculadoura.valorDaSubtração();
+            calculadoura.valorDeMultiplicação(5,20);
+    }
+}
